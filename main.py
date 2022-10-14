fd2b37a50b38dec9a1cd6ce12e83f593fd4b521c
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2022-10-14 18:22:49 +0000

    experiment with floats and typcasting/conversion

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #Data Types
 
-num_char = len(input("What is your name?"))
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
 
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + " characters.")
+a = float(123)
+print(type(a))
 
+print(70 + float("100.5"))
